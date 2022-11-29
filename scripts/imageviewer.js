87e4330f26b4565c5c0ba03524c704a6ef993f16
Jadquir <jadquircontactgmail.com>
2022-11-29 22:11:11 +0300

    added details to image gallery

--- scripts/imageviewer.js
+++ scripts/imageviewer.js
@@ -4,17 +4,28 @@ $(document).ready(function() {
     var imgPopup = $('.img-popup');
     var imgCont  = $('img');
     var popupImage = $('.img-popup img');
+    var detailsTitle = $('.img-popup .details .Title');
+    var detailsDescription = $('.img-popup .details .Description');
     var closeBtn = $('.close-btn');
-            
+  
     // handle events
-    imgCont.on('click', function() {
+    imgCont.on('click', function(event) {
       var img_src = $(this).attr('src');
       img_src = img_src.replace("low_res","high_res");
       imgPopup.children('img').attr('src', img_src);
+      try{
+        detailsTitle.text($(event.target)[0].dataset.atitle);
+        detailsDescription.text($(event.target)[0].dataset.description);
+  
+      }
+      catch(e){
+          console.error(e);
+      }
+      
       imgPopup.addClass('opened');
     });
     
-    $(imgPopup, closeBtn).on('click', function() {
+    $(closeBtn).on('click', function() {
       imgPopup.removeClass('opened');
       imgPopup.children('img').attr('src', '');
     });
