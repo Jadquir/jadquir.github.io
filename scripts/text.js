7e33d760c372d868a03c167eaef98988650fbe98
Emircan Gezer <63879949+Jadquir@users.noreply.github.com>
2024-01-11 23:41:03 +0300

    Update text.js

--- scripts/text.js
+++ scripts/text.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ['a Student ', 'a Freelancer '];
+const textArray = ['a Student ', 'a GameDev ','a Freelancer '];
 const typingDelay = 150;
 const erasingDelay = 100;
 const newTextDelay = 1500; // Delay between current and next text
